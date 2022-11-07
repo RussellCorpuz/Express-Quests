863f560be833462c8252cfff7bb8de1de29a86b6
RussellCorpuz <russelljames.fr@icloud.com>
2022-11-07 17:39:18 +0100

    commit 4

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 const port = process.env.APP_PORT ?? 4000;
 
 const welcome = (req, res) => {
@@ -17,6 +19,8 @@ const userHandlers = require("./userHandlers");
 app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUserById);
 
+app.post("/api/users", userHandlers.postUser);
+
 app.listen(port, (err) => {
   if (err) {
     console.error("Something bad happened");
@@ -25,29 +29,9 @@ app.listen(port, (err) => {
   }
 });
 
-
-
-//const express = require("express");
-
-//const app = express();
-
-//const port = process.env.APP_PORT ?? 4000;
-
-//const welcome = (req, res) => {
-//  res.send("Welcome to my favourite movie list");
-//};
-
-//app.get("/", welcome);
-
 const movieHandlers = require("./movieHandlers");
 
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
 
-//app.listen(port, (err) => {
-//  if (err) {
-//    console.error("Something bad happened");
-//  } else {
-//    console.log(`Server is listening on ${port}`);
-//  }
-//});
+app.post("/api/movies", movieHandlers.postMovie);
--- movieHandlers.js
+++ movieHandlers.js
@@ -1,31 +1,27 @@
-/*const movies = [
-  {
-    id: 1,
-    title: "Citizen Kane",
-    director: "Orson Wells",
-    year: "1941",
-    colors: false,
-    duration: 120,
-  },
-  {
-    id: 2,
-    title: "The Godfather",
-    director: "Francis Ford Coppola",
-    year: "1972",
-    colors: true,
-    duration: 180,
-  },
-  {
-    id: 3,
-    title: "Pulp Fiction",
-    director: "Quentin Tarantino",
-    year: "1994",
-    color: true,
-    duration: 180,
-  },
-];*/
-
 const database = require("./database");
+
+const postMovie = (req, res) => {
+  const { title, director, year, color, duration } = req.body;
+
+  database
+    .query(
+      "INSERT INTO movies(title, director, year, color, duration) VALUES (?, ?, ?, ?, ?)",
+      [title, director, year, color, duration]
+    )
+    .then(([result]) => {
+      res.location(`/api/movies/${result.insertId}`).sendStatus(201);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error saving the movie");
+    });
+};
+
+
+/*const postMovie = (req, res) => {
+    console.log(req.body);
+    res.send("Post route is working 🎉");
+  };*/
 
 const getMovies = (req, res) => {
   database
@@ -60,4 +56,7 @@ const getMovieById = (req, res) => {
 module.exports = {
   getMovies,
   getMovieById,
+  postMovie,
 };
+
+
