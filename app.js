278c9feb75d4db55405f87c5d2d99cb373fe7143
RussellCorpuz <russelljames.fr@icloud.com>
2022-11-08 11:26:25 +0100

    solution express 04 PUT

--- app.js
+++ app.js
@@ -21,6 +21,8 @@ app.get("/api/users/:id", userHandlers.getUserById);
 
 app.post("/api/users", userHandlers.postUser);
 
+app.put("/api/users/:id", userHandlers.updateUser);
+
 app.listen(port, (err) => {
   if (err) {
     console.error("Something bad happened");
@@ -35,3 +37,7 @@ app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
 
 app.post("/api/movies", movieHandlers.postMovie);
+
+app.put("/api/movies/:id", movieHandlers.updateMovie);
+
+
--- movieHandlers.js
+++ movieHandlers.js
@@ -1,5 +1,29 @@
 const database = require("./database");
 
+const updateMovie = (req, res) => {
+  const id = parseInt(req.params.id);
+  const { title, director, year, color, duration } = req.body;
+
+  database
+    .query(
+      "update movies set title = ?, director = ?, year = ?, color = ?, duration = ? where id = ?",
+      [title, director, year, color, duration, id]
+    )
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.status(404).send("Not Found");
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error editing the movie");
+    });
+};
+
+
+
 const postMovie = (req, res) => {
   const { title, director, year, color, duration } = req.body;
 
@@ -16,12 +40,6 @@ const postMovie = (req, res) => {
       res.status(500).send("Error saving the movie");
     });
 };
-
-
-/*const postMovie = (req, res) => {
-    console.log(req.body);
-    res.send("Post route is working 🎉");
-  };*/
 
 const getMovies = (req, res) => {
   database
@@ -57,6 +75,6 @@ module.exports = {
   getMovies,
   getMovieById,
   postMovie,
+  updateMovie,
 };
-
 
